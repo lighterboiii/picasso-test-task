6dfcb08d554b0ff7584d51cc9b77455b072e1017
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-01-24 19:36:10 +0300

    initial commit

---
+++ src/components/api/api.ts
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const postsApi = createApi({
+  reducerPath: 'postsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+  endpoints: (builder) => ({
+    getPosts: builder.query<any[], void>({
+      query: () => 'posts',
+    }),
+    getPostById: builder.query<any, number>({
+      query: (id) => `posts/${id}`,
+    }),
+  }),
+});
+
+export const { useGetPostsQuery, useGetPostByIdQuery } = postsApi;
